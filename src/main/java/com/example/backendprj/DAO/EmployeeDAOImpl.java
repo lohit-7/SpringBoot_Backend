535990f9f136b40cf22515b66345d24df3dbac37
lohit-7 <naiklohit743@gmail.com>
2025-04-04 10:38:21 +0530

    employee_DAO

--- src/main/java/com/example/backendprj/DAO/EmployeeDAOImpl.java
+++ src/main/java/com/example/backendprj/DAO/EmployeeDAOImpl.java
@@ -1,4 +1,37 @@
 package com.example.backendprj.DAO;
 
+import com.example.backendprj.domain.EmployeeEntity;
+import com.example.backendprj.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class EmployeeDAOImpl implements EmployeeDAO{
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public EmployeeEntity getAllEmployee() {
+        return null;
+    }
+
+    @Override
+    public List<EmployeeEntity> getAllEmpById(Long empId) {
+        return List.of();
+    }
+
+    @Override
+    public EmployeeEntity addEmployee(EmployeeEntity employeeEntity) {
+        return null;
+    }
+
+    @Override
+    public EmployeeEntity UpdateEmployee(EmployeeEntity employeeEntity) {
+        return null;
+    }
+
+    @Override
+    public EmployeeEntity deleteEmp(Long empId) {
+        return null;
+    }
 }
