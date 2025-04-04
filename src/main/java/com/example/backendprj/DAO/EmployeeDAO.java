c7ef7d05acd4ccd373aad60b1e16b0e044a7c6ae
lohit-7 <naiklohit743@gmail.com>
2025-04-04 10:28:03 +0530

    employee_DAO

--- src/main/java/com/example/backendprj/DAO/EmployeeDAO.java
+++ src/main/java/com/example/backendprj/DAO/EmployeeDAO.java
@@ -1,4 +1,17 @@
 package com.example.backendprj.DAO;
 
+import java.util.*;
+import com.example.backendprj.domain.EmployeeEntity;
+
 public interface EmployeeDAO {
+
+    EmployeeEntity getAllEmployee();
+
+    List<EmployeeEntity> getAllEmpById(Long empId);
+
+    EmployeeEntity addEmployee(EmployeeEntity employeeEntity);
+
+    EmployeeEntity UpdateEmployee(EmployeeEntity employeeEntity);
+
+    EmployeeEntity deleteEmp(Long empId);
 }
