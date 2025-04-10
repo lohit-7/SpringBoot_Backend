74d06864fa8f5222f603627a540f5b9f4db33a1e
lohit-7 <naiklohit743@gmail.com>
2025-04-10 11:21:51 +0530

    empmodel1

---
+++ src/main/java/com/example/backendprj/model/StaffModel.java
@@ -0,0 +1,34 @@
+package com.example.backendprj.model;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+public class StaffModel {
+
+    private  Long empId;
+
+    @NotNull(message = "not be empty")
+    private String firstName;
+
+    @NotNull(message = "enter this field")
+    private String lastName;
+
+    @NotNull(message = "enter this field")
+    private String email;
+
+    @NotNull(message = "enter this field")
+    private String phoneNumber;
+
+    @NotNull(message = "enter this field")
+    private String jobRole;
+
+    @NotNull(message = "enter this field")
+    private String jobType;
+}
